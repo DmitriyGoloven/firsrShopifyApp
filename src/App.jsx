585da61624957c125ce180bb46d21afb608aa1c5
DmitriyHoloven <goloven.dmitriy@gmail.com>
2022-05-12 04:53:24 +0300

    ytro

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { Redirect } from "@shopify/app-bridge/actions";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import translations from "@shopify/polaris/locales/en.json";
 import "@shopify/polaris/build/esm/styles.css";
-import {ProductsPageNew} from "./components/ProductsPageNew";
+import {ProductsPagePagination} from "./components/ProductsPagePaginatin";
 
 
 export default function App() {
@@ -28,7 +28,7 @@ export default function App() {
             }}
         >
           <MyProvider>
-            <ProductsPageNew/>
+            <ProductsPagePagination/>
           </MyProvider>
         </AppBridgeProvider>
       </PolarisProvider>
---
+++ src/components/ProductsPagePaginatin.jsx
@@ -0,0 +1,114 @@
+import {gql, useLazyQuery, useQuery} from "@apollo/client";
+import {Page, Layout, Banner, Card, Pagination} from "@shopify/polaris";
+import {Loading} from "@shopify/app-bridge-react";
+import {ProductsList} from "./ProductsList";
+import {useEffect} from "react";
+
+const GET_PRODUCTS = gql`
+  query GetProducts($count: Int) {
+  products(first: $count) {
+    edges {
+      cursor
+      node {
+        title
+        status
+        handle
+        descriptionHtml
+        id
+        images(first: 1) {
+          edges {
+            node {
+              id
+              originalSrc
+              altText
+            }
+          }
+        }
+        variants(first: 1) {
+          edges {
+            node {
+              position
+              weight
+              price
+              id
+            }
+          }
+        }
+      }
+    }
+    pageInfo {
+      endCursor
+      hasNextPage
+      hasPreviousPage
+      startCursor
+    }
+  }
+}
+`;
+
+export function ProductsPagePagination() {
+    const PRODUCTS_COUNT = 3
+    const {loading, error, data} = useQuery(GET_PRODUCTS, {
+        variables: {count: PRODUCTS_COUNT},
+    });
+    //  const [getProducts, {loading, error, data}] = useLazyQuery(GET_PRODUCTS,{
+    //         variables: {count: PRODUCTS_COUNT},
+    //       })
+    // getProducts().then((r) =>{console.log(r)})
+
+    console.log(data)
+    if (loading) return <Loading/>;
+
+    if (error) {
+        console.warn(error);
+        return (
+            <Banner status="critical">There was an issue loading products.</Banner>
+        );
+    }
+
+    // useEffect(() => {
+    //     getProducts({
+    //         variables: {
+    //             count: PRODUCTS_COUNT,
+    //         }
+    //     });
+    // }, []);
+
+    // const nextPage = () => {
+    //
+    //     getProducts({
+    //         variables: {
+    //             count: PRODUCTS_COUNT
+    //
+    //         }
+    //     });
+
+    //     const previousPage = () => {
+    //         getProducts({
+    //             variables: {
+    //                 count: PRODUCTS_COUNT
+    //             }
+    //         });
+    //     }
+
+    return (
+
+        <Page>
+            <Layout>
+                <Layout.Section>
+                    <Pagination
+                        // hasPrevious={data.products.pageInfo.hasPreviousPage}
+                        // onPrevious={previousPage}
+                        // hasNext={data.products.pageInfo.hasNextPage}
+                        // onNext={nextPage}
+                    />
+                    <Card>
+                        <ProductsList data={data}/>
+                    </Card>
+
+                </Layout.Section>
+            </Layout>
+        </Page>
+    );
+}
+
